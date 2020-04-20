ce0aeb827527abed4ce8f5094d25e17d82b64156
Cilvia333 <cilvia333x@gmail.com>
2020-04-21 07:06:21 +0900

    Update card background

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,7 +13,6 @@ type animType = {
   color: string;
   radius: number;
   alpha?: number;
-  lineWidth: number;
   draw: Function;
 };
 
@@ -26,163 +25,99 @@ type Props = {
 
 export default function Card({ name, link, size, logo }: Props) {
   const [isMouseOver, setIsMouseOver] = useState(false);
+  const [circle, setCircle] = useState<animType>();
   const logoWithProps = React.cloneElement(logo, {
     size: size,
     isAnimation: isMouseOver,
   });
   const cardRef = React.createRef<HTMLDivElement>();
-  const backgroundRef = React.createRef<HTMLDivElement>();
   const { elX, elY, elW, elH } = useMouse(cardRef);
   const canvasRef = React.createRef<HTMLCanvasElement>();
-  let ctx: CanvasRenderingContext2D | null;
-  let timeline: anime.AnimeTimelineInstance;
-  let timeline2: anime.AnimeTimelineInstance;
-  const createCircle = (x, y, radius) => {
+  //let ctx: CanvasRenderingContext2D | null;
+  const checkRange = (value: number, ref: number): number => {
+    if (value < 0) return 0;
+    else if (value > ref) return ref;
+    else return value;
+  };
+  const createCircle = (x: number, y: number, radius: number) => {
+    const canvas = canvasRef.current;
+    if (!canvas || canvas === undefined) return;
+    const ctx = canvas.getContext('2d');
     const p: animType = {
       x: x,
       y: y,
-      color: '#FFF',
-      radius: radius,
-      lineWidth: 0,
+      color: Colors.orange,
+      radius: 0,
       draw: function() {
         if (!ctx) return;
-        ctx.globalAlpha = p.alpha;
-        ctx?.beginPath();
-        ctx?.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
         ctx.fillStyle = p.color;
         ctx.fill();
-        //ctx.lineWidth = p.lineWidth;
-        //ctx.strokeStyle = p.color;
-        //ctx?.stroke();
-        //ctx.globalAlpha = 1;
       },
     };
     return p;
   };
 
-  const animateCircle = (x: number, y: number) => {
-    if (isMouseOver) {
-      const circle = createCircle(x, y, 0.1);
-      timeline2 = anime.timeline().add({
-        targets: circle,
-        radius: 800,
-        duration: 300,
-        easing: 'easeOutExpo',
-        update: anim => {
-          for (let i = 0; i < anim.animatables.length; i++) {
-            anim.animatables[i].target.draw();
-          }
-        },
-        offset: 0,
-      });
-    } else {
-      const circle = createCircle(x, y, 800);
-      timeline2 = anime.timeline().add({
-        targets: circle,
-        radius: 0,
-        duration: 300,
-        easing: 'easeInExpo',
-        update: anim => {
-          for (let i = 0; i < anim.animatables.length; i++) {
-            anim.animatables[i].target.draw();
-          }
-        },
-        offset: 0,
-      });
-    }
+  const expandCircle = (x: number, y: number) => {
+    anime.timeline().add({
+      targets: circle,
+      x: { value: x, duration: 0 },
+      y: { value: y, duration: 0 },
+      radius: [0, 200],
+      duration: 300,
+      easing: 'easeInExpo',
+      update: anim => {
+        for (let i = 0; i < anim.animatables.length; i++) {
+          anim.animatables[i].target.draw();
+        }
+      },
+    });
+  };
+
+  const closeCircle = (x: number, y: number) => {
+    anime.timeline().add({
+      targets: circle,
+      x: { value: x, duration: 0 },
+      y: { value: y, duration: 0 },
+      radius: [200, 0],
+      duration: 300,
+      easing: 'easeOutExpo',
+      update: anim => {
+        for (let i = 0; i < anim.animatables.length; i++) {
+          anim.animatables[i].target.draw();
+        }
+      },
+    });
   };
   useEffect(() => {
     const canvas = canvasRef.current;
 
-    if (canvas) {
-      ctx = canvas?.getContext('2d');
+    if (canvas && canvas !== undefined) {
       canvas.width = elW;
       canvas.height = elH;
       canvas.style.width = `${elW}px`;
       canvas.style.height = `${elH}px`;
+      setCircle(createCircle(0, 0, 0.1));
     }
-  });
-
-  useEffect(() => {
-    animateCircle(elX, elY);
-    if (isMouseOver) {
-      timeline = anime
-        .timeline({
-          targets: backgroundRef.current,
-          autoplay: false,
-        })
-        .add({
-          translateX: {
-            value: `${elX}px`,
-            duration: 0,
-          },
-          translateY: {
-            value: `${elY}px`,
-            duration: 0,
-          },
-          scale: {
-            value: [0, 10],
-            duration: 200,
-            delay: 0,
-            easing: 'easeInOutQuart',
-          },
-          opacity: {
-            value: [0, 1],
-            duration: 200,
-            delay: 0,
-            easing: 'easeInOutQuart',
-          },
-        });
-    } else {
-      timeline = anime
-        .timeline({
-          targets: backgroundRef.current,
-          autoplay: false,
-        })
-        .add({
-          translateX: {
-            value: `${elX}px`,
-            duration: 0,
-          },
-          translateY: {
-            value: `${elY}px`,
-            duration: 0,
-          },
-          scale: {
-            value: [10, 0],
-            duration: 200,
-            delay: 0,
-            easing: 'easeInOutQuart',
-          },
-          opacity: {
-            value: [1, 0],
-            duration: 200,
-            delay: 0,
-            easing: 'easeInOutQuart',
-          },
-        });
-    }
-  }, [isMouseOver]);
-
-  useEffect(() => {
-    //timeline.play();
-  }, [isMouseOver]);
+  }, [elW, elH]);
 
   return (
     <>
-      <CardLink
-        to={`/content/${link}/`}
-        onMouseEnter={e => {
-          setIsMouseOver(true);
-        }}
-        onMouseLeave={e => {
-          setIsMouseOver(false);
-        }}
-      >
-        <Wrapper>
+      <CardLink to={`/content/${link}/`}>
+        <Wrapper
+          onMouseEnter={e => {
+            expandCircle(checkRange(elX, elW), checkRange(elY, elH));
+            setIsMouseOver(true);
+          }}
+          onMouseLeave={e => {
+            closeCircle(checkRange(elX, elW), checkRange(elY, elH));
+            setIsMouseOver(false);
+          }}
+        >
           <Canvas ref={canvasRef} />
           <ContentWrapper ref={cardRef}>
-            {/* <BackGround size={size} ref={backgroundRef} /> */}
             {logoWithProps}
             <Title>{name}</Title>
           </ContentWrapper>
@@ -198,7 +133,7 @@ const CardLink = styled(Link)`
 `;
 
 const Wrapper = styled.div`
-  background-color: ${Colors.gray};
+  /* background-color: ${Colors.gray}; */
   height: 100%;
   width: 100%;
   position: relative;
@@ -213,22 +148,6 @@ const ContentWrapper = styled.div`
   width: 100%;
   position: relative;
   overflow: hidden;
-`;
-
-const BackGround = styled.div<{ size: number }>`
-  position: absolute;
-  background-color: ${Colors.red};
-  top: 0;
-  left: 0;
-  border-radius: 50%;
-  transform: scale(0);
-  opacity: 0;
-
-  ${({ size }) =>
-    css`
-      height: ${size}px;
-      width: ${size}px;
-    `}
 `;
 
 const Title = styled.div`
